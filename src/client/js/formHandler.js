daf6696850af7888b6b208dc25f754de83652015
Engy <engyyyibrahimmm@gmail.com>
2021-10-10 23:43:08 +0200

    add sentiment analysis api

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -6,10 +6,44 @@ function handleSubmit(event) {
     Client.checkForName(formText)
 
     console.log("::: Form Submitted :::")
-    fetch('http://localhost:8081/test')
+    // fetch('http://localhost:8081/test')
+    // .then(res => res.json())
+    // .then(function(res) {
+    //     document.getElementById('results').innerHTML = res.message
+    // })
+    fetch('http://localhost:8081/submit', {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({url: formText})
+    })
     .then(res => res.json())
-    .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
+    .then(({agreement, confidence, subjectivity, irony, score_tag}) => {
+        const result = document.getElementById('results')
+        const list = document.createElement('ul')
+
+        let li = document.createElement('li')
+        li.textContent = `agreement: ${agreement}`
+        list.appendChild(li)
+
+        li = document.createElement('li')
+        li.textContent = `confidence: ${confidence}`
+        list.appendChild(li)
+
+        li = document.createElement('li')
+        li.textContent = `subjectivity: ${subjectivity}`
+        list.appendChild(li)
+
+        li = document.createElement('li')
+        li.textContent = `irony: ${irony}`
+        list.appendChild(li)
+
+        li = document.createElement('li')
+        li.textContent = `polarity: ${score_tag}`
+        list.appendChild(li)
+
+        result.appendChild(list)
     })
 }
 
--- src/server/index.js
+++ src/server/index.js
@@ -1,12 +1,18 @@
+const dotenv = require('dotenv')
+dotenv.config()
+
 var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
 const cors = require('cors')
+const bodyParser = require('body-parser')
+const fetch = require('node-fetch')
 
 const app = express()
 
 app.use(express.static('dist'))
 app.use(cors())
+app.use(bodyParser.json())
 
 console.log(__dirname)
 
@@ -19,7 +25,19 @@ app.listen(8081, function () {
     console.log('Example app listening on port 8081!')
 })
 
-app.get('/test', function (req, res) {
-    //res.header('Access-Control-Allow-Origin', 'http://localhost:8080')
-    res.send(mockAPIResponse)
+// app.get('/test', function (req, res) {
+//     //res.header('Access-Control-Allow-Origin', 'http://localhost:8080')
+//     res.send(mockAPIResponse)
+// })
+
+app.post('/submit', function(req,res) {
+  console.log(req.body)
+  fetch(`https://api.meaningcloud.com/sentiment-2.1?key=${process.env.API_KEY}&&lang=en&&url=${req.body.url}`, {
+    method: 'POST',
+  })
+  .then(data => data.json())
+  .then(data => {
+    console.log(data)
+    res.send(data)
+  })
 })
